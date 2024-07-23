2d79a0cea105c32fdca3446264068564542eeb33
Alexander <kkruscherr@gmail.com>
2024-07-23 23:24:41 +0300

    Up to Sprint 14; Lesson 04
    Custom Pagination

--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,15 @@
-from rest_framework import viewsets
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import permissions, viewsets
 
 from .models import Achievement, Cat, User
-from .permissions import OwnerOrReadOnly
+from .pagination import CatsPagination
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    permission_classes = (OwnerOrReadOnly,)
-    pagination_class = LimitOffsetPagination
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    pagination_class = CatsPagination
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
