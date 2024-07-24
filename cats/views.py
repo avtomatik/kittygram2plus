99199e4801093a99b0b269785afc863c94c24a4e
Alexander <kkruscherr@gmail.com>
2024-07-24 19:18:19 +0300

    Up to Sprint 14; Lesson 05

--- cats/views.py
+++ cats/views.py
@@ -1,15 +1,23 @@
-from rest_framework import permissions, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
 from .models import Achievement, Cat, User
-from .pagination import CatsPagination
+from .permissions import OwnerOrReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-    pagination_class = CatsPagination
+    permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (
+        DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter
+    )
+    pagination_class = None
+    filtered_fields = ('color', 'birth_year')
+    search_fields = ('^name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
